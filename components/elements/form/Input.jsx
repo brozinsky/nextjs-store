5fac66b4f447cbd8b065bc7bf0726ecdd4e15ea6
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2022-11-29 23:54:59 +0100

    chore(form): add clsx, apply to form inputs

--- components/elements/form/Input.jsx
+++ components/elements/form/Input.jsx
@@ -1,29 +1,29 @@
+import clsx from "clsx";
+
 const Input = ({ name, required, placeholder, label }) => {
-  const isError = false;
+  const isError = true;
   return (
     <label
-      className={`${
-        isError ? "text-red-700" : ""
-      } block mb-2 text-sm font-medium  `}
+      className={clsx("block mb-2 text-sm font-medium", {
+        "text-red-700": isError,
+      })}
     >
       <span className="inline-block mb-2">
         {label}
         {required ? "*" : null}
       </span>
       <input
-        className={`${
-          isError
-            ? "border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 bg-red-50"
-            : ""
-        } text-sm block w-full px-5 py-2.5 bg-white border-slate-300 border focus:ring-primary-100`}
+        className={clsx("text-sm block w-full px-5 py-2.5 border", {
+          "border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 bg-red-50":
+            isError,
+          "border-slate-300 focus:ring-primary-100 bg-white": !isError,
+        })}
         name={name}
         type="text"
         placeholder={placeholder}
       />
       {isError ? (
-        <p className="mt-2 ml-2 text-sm text-red-600 dark:text-red-500">
-          Username available!
-        </p>
+        <p className="mt-2 ml-2 text-sm text-red-600">Username available!</p>
       ) : null}
     </label>
   );
--- components/elements/form/Textarea.jsx
+++ components/elements/form/Textarea.jsx
@@ -1,30 +1,30 @@
+import clsx from "clsx";
+
 const Textarea = ({ name, required, placeholder, label }) => {
   const isError = true;
   return (
     <label
-      className={`${
-        isError ? "text-red-700" : ""
-      } block mb-2 text-sm font-medium col-start-1 col-end-3`}
+      className={clsx("block mb-2 text-sm font-medium col-start-1 col-end-3", {
+        "text-red-700": isError,
+      })}
     >
       <span className="inline-block mb-2">
         {label}
         {required ? "*" : null}
       </span>
       <textarea
-        className={`border border-slate-300 ${
-          isError
-            ? "border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 bg-red-50"
-            : ""
-        } text-sm block w-full px-5 py-2.5 bg-white resize-none`}
+        className={clsx("text-sm block w-full px-5 py-2.5 border resize-none", {
+          "border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 bg-red-50":
+            isError,
+          "border-slate-300 focus:ring-primary-100 bg-white": !isError,
+        })}
         name={name}
         placeholder={placeholder}
         cols="30"
         rows="6"
       ></textarea>
       {isError ? (
-        <p className="mt-2 ml-2 text-sm text-red-600 dark:text-red-500">
-          Username available!
-        </p>
+        <p className="mt-2 ml-2 text-sm text-red-600">Username available!</p>
       ) : null}
     </label>
   );
